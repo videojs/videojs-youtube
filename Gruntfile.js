c0234638f18f9cb85716e709743b13f3f98dd713
Benoit Tremblay <trembl.ben@gmail.com>
2014-04-26 14:05:26 -0400

    Typos

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,15 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     uglify: {
-	  vjsyoutube: {
-	    options: {
-		  mangle: false
-		},
-	    files: {
-		  'dist/vjs.youtube.js': ['src/youtube.js']
-		}
-	  }
-	}
+      vjsyoutube: {
+        options: {
+          mangle: false
+        },
+        files: {
+          'dist/vjs.youtube.js': ['src/youtube.js']
+        }
+      }
+    }
   });
   
   grunt.loadNpmTasks('grunt-contrib-uglify');
