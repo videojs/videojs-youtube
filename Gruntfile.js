cee64295109a741379e5e531668629c12344c6fc
Benoit Tremblay <trembl.ben@gmail.com>
2014-04-26 14:53:44 -0400

    Added component.json and grunt release

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,18 @@ module.exports = function(grunt) {
           'dist/vjs.youtube.js': ['src/youtube.js']
         }
       }
+    },
+    release: {
+      file: 'package.json',
+      commit:  true,
+      npm: true,
+      message: 'Release %version%',
+      prefix:  'v'
     }
   });
   
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-release');
   
   grunt.registerTask('default', ['uglify']);
 };
