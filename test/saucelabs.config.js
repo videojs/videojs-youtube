b5f33976999cf3fd484d11162e228d4626e6118d
Benoit Tremblay <trembl.ben@gmail.com>
2014-05-29 06:18:55 -0400

    Added multiple browsers to test with SauceLabs

--- test/saucelabs.config.js
+++ test/saucelabs.config.js
@@ -1,13 +1,42 @@
 exports.config = {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
-
-  capabilities: {
-    'browserName': 'chrome',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Chrome'
-  },
+  
+  multiCapabilities: [{
+        browserName: 'firefox',
+        version: '19',
+        platform: 'XP',
+        build: process.env.TRAVIS_BUILD_NUMBER,
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+    }, {
+        browserName: 'chrome',
+        platform: 'XP',
+        build: process.env.TRAVIS_BUILD_NUMBER,
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+    }, {
+        browserName: 'chrome',
+        platform: 'linux',
+        build: process.env.TRAVIS_BUILD_NUMBER,
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+    }, {
+        browserName: 'internet explorer',
+        platform: 'WIN8',
+        version: '10',
+        build: process.env.TRAVIS_BUILD_NUMBER,
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+    }, {
+        browserName: 'internet explorer',
+        platform: 'VISTA',
+        version: '9',
+        build: process.env.TRAVIS_BUILD_NUMBER,
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+    }, {
+        browserName: 'internet explorer',
+        platform: 'XP',
+        version: '8',
+        build: process.env.TRAVIS_BUILD_NUMBER,
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+    }],
   
   baseUrl: 'http://localhost:8080',
 
