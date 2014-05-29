769f0752a28127ec18f0f0cc79d1e817511c028c
Benoit Tremblay <trembl.ben@gmail.com>
2014-05-29 06:25:35 -0400

    Added names to SauceLabs test

--- test/saucelabs.config.js
+++ test/saucelabs.config.js
@@ -3,34 +3,40 @@ exports.config = {
   sauceKey: process.env.SAUCE_ACCESS_KEY,
   
   multiCapabilities: [{
+        name: 'FireFox on XP',
         browserName: 'firefox',
         version: '19',
         platform: 'XP',
         build: process.env.TRAVIS_BUILD_NUMBER,
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
     }, {
+        name: 'Chrome on XP',
         browserName: 'chrome',
         platform: 'XP',
         build: process.env.TRAVIS_BUILD_NUMBER,
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
     }, {
+        name: 'Chrome on Linux',
         browserName: 'chrome',
         platform: 'linux',
         build: process.env.TRAVIS_BUILD_NUMBER,
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
     }, {
+        name: 'IE10 on Windows 8',
         browserName: 'internet explorer',
         platform: 'WIN8',
         version: '10',
         build: process.env.TRAVIS_BUILD_NUMBER,
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
     }, {
+        name: 'IE9 on Visa',
         browserName: 'internet explorer',
         platform: 'VISTA',
         version: '9',
         build: process.env.TRAVIS_BUILD_NUMBER,
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
     }, {
+        name 'IE8 on XP',
         browserName: 'internet explorer',
         platform: 'XP',
         version: '8',
