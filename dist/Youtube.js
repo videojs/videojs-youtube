e73db09d64ef64e4ca8c9b6cc8a7e00f55fb3271
Gary Katsevman <git@gkatsev.com>
2016-12-12 11:47:33 -0500

    2.2.0

--- dist/Youtube.js
+++ dist/Youtube.js
@@ -206,6 +206,14 @@ THE SOFTWARE. */
         playerVars.theme = this.options_.theme;
       }
 
+      // Allow undocumented options to be passed along via customVars
+      if (typeof this.options_.customVars !== 'undefined') {
+        var customVars = this.options_.customVars;
+        Object.keys(customVars).forEach(function(key) {
+          playerVars[key] = customVars[key];
+        });
+      }
+
       this.activeVideoId = this.url ? this.url.videoId : null;
       this.activeList = playerVars.list;
 
@@ -222,6 +230,10 @@ THE SOFTWARE. */
     },
 
     onPlayerReady: function() {
+      if (this.options_.muted) {
+        this.ytPlayer.mute();
+      }
+
       this.playerReady_ = true;
       this.triggerReady();
 
