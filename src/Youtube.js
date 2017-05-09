280a654ae4ce37d6d39eaf7a53c7f3b1c289d95f
Dino Bettini <dbettini91@gmail.com>
2017-05-10 00:07:44 +0200

    feat: Trigger volumechange in YT.Player event listener (#439)
    volumechange tech event is now triggered in onVolumeChange
    event listener, after youtube player emits the event,
    instead of delaying the trigger for magic value of 50ms

--- src/Youtube.js
+++ src/Youtube.js
@@ -232,6 +232,7 @@ THE SOFTWARE. */
           onPlaybackQualityChange: this.onPlayerPlaybackQualityChange.bind(this),
           onPlaybackRateChange: this.onPlayerPlaybackRateChange.bind(this),
           onStateChange: this.onPlayerStateChange.bind(this),
+          onVolumeChange: this.onPlayerVolumeChange.bind(this),
           onError: this.onPlayerError.bind(this)
         }
       });
@@ -312,6 +313,10 @@ THE SOFTWARE. */
           this.player_.trigger('waiting');
           break;
       }
+    },
+
+    onPlayerVolumeChange: function() {
+      this.trigger('volumechange');
     },
 
     onPlayerError: function(e) {
@@ -574,10 +579,6 @@ THE SOFTWARE. */
       }
 
       this.ytPlayer.setVolume(percentAsDecimal * 100.0);
-      this.setTimeout( function(){
-        this.trigger('volumechange');
-      }, 50);
-
     },
 
     muted: function() {
